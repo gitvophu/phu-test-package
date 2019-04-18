9d7ad005d8cc9a9f23dca8df29d9c5e1bf769dc8
Phu <vongocphu04@gmail.com>
2019-04-18 14:32:02 +0700

    first

---
+++ src/ContactFormServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+    // MyVendor\contactform\src\ContactFormServiceProvider.php
+    namespace MyVendor\contactform;
+    use Illuminate\Support\ServiceProvider;
+    class ContactFormServiceProvider extends ServiceProvider {
+        public function boot()
+        {
+        }
+        public function register()
+        {
+        }
+    }
